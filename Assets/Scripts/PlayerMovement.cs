3a4c55216e87741dce231e68bdfedbca8f4e571e
Aurelien Tavernier <aurelientav@gmail.com>
2023-11-24 09:20:16 +0100

    move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float speed;
+    int Direction;
     void Start()
     {
-        
+        speed = 0;
+        Direction = 1;
+        OnMove();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if(speed > 0)
+        {
+            Vector2 velocite = new Vector2 (speed * Time.deltaTime, 0);
+            transform.Translate(velocite, Space.World);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("d");
+        if(collision.gameObject.tag == "Wall")
+        {
+            speed = 0;
+            if(Direction > 0)
+            {
+                Direction = -1;
+            }
+            else
+            {
+                Direction = 1;
+            }
+        }
+    }
+
+    void OnMove()
+    {
+        speed = 5;
     }
 }
