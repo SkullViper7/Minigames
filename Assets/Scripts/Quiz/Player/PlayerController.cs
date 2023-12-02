5f4587f85bfbe7bedf6844bd78fb76d3f70af083
Viper <41963246+SkullViper7@users.noreply.github.com>
2023-12-02 19:58:26 +0100

    logo movement

--- Assets/Scripts/Quiz/Player/PlayerController.cs
+++ Assets/Scripts/Quiz/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     [Space]
     public PlayerInput playerInput;
+    public LogoMove logoMove;
 
     [Header("Answers")]
     public int answerChosed;
@@ -118,20 +119,88 @@ public class PlayerController : MonoBehaviour
     void AnswerOneChoose()
     {
         answerChosed = 1;
+
+        if (gameObject.tag == "Player1")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player1, logoMove.AHolder1));
+        }
+        if (gameObject.tag == "Player2")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player2, logoMove.AHolder2));
+        }
+        if (gameObject.tag == "Player3")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player3, logoMove.AHolder3));
+        }
+        if (gameObject.tag == "Player4")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player4, logoMove.AHolder4));
+        }
     }
 
     void AnswerTwoChoose()
     {
         answerChosed = 2;
+
+        if (gameObject.tag == "Player1")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player1, logoMove.BHolder1));
+        }
+        if (gameObject.tag == "Player2")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player2, logoMove.BHolder2));
+        }
+        if (gameObject.tag == "Player3")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player3, logoMove.BHolder3));
+        }
+        if (gameObject.tag == "Player4")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player4, logoMove.BHolder4));
+        }
     }
 
     void AnswerThreeChoose()
     {
         answerChosed = 3;
+
+        if (gameObject.tag == "Player1")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player1, logoMove.XHolder1));
+        }
+        if (gameObject.tag == "Player2")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player2, logoMove.XHolder2));
+        }
+        if (gameObject.tag == "Player3")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player3, logoMove.XHolder3));
+        }
+        if (gameObject.tag == "Player4")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player4, logoMove.XHolder4));
+        }
     }
 
     void AnswerFourChoose()
     {
         answerChosed = 4;
+
+        if (gameObject.tag == "Player1")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player1, logoMove.YHolder1));
+        }
+        if (gameObject.tag == "Player2")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player2, logoMove.YHolder2));
+        }
+        if (gameObject.tag == "Player3")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player3, logoMove.YHolder3));
+        }
+        if (gameObject.tag == "Player4")
+        {
+            StartCoroutine(logoMove.Move(logoMove.player4, logoMove.YHolder4));
+        }
     }
 }
