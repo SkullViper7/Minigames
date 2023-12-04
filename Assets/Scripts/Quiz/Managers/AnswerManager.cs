84fd6d02e5635331da7db7e69639d8a410ca3d5e
Viper <drandran650@gmail.com>
2023-12-04 19:40:51 +0100

    animation + answer fix

--- Assets/Scripts/Quiz/Managers/AnswerManager.cs
+++ Assets/Scripts/Quiz/Managers/AnswerManager.cs
@@ -11,6 +11,7 @@ public class AnswerManager : MonoBehaviour
     public ScoreManager scoreManager;
     public QuestionManager questionManager;
     public AnswerTextManager answerTextManager;
+    public CorrectAnswerBlink blink;
 
     GameObject player1;
     GameObject player2;
@@ -41,13 +42,15 @@ public class AnswerManager : MonoBehaviour
         Invoke("AnswerCheck", 10);
     }
 
-    private void Update()
+    public void UpdateCorrectAnswer()
     {
         correctAnswer = correctAnswers[questionManager.questionPicked];
     }
 
     public void AnswerCheck()
     {
+        blink.pause = true;
+
         if (GameManager.Instance.maxPlayerCount == 2)
         {
             if (player1.GetComponent<PlayerController>().answerChosed == correctAnswer)
@@ -102,13 +105,32 @@ public class AnswerManager : MonoBehaviour
             }
         }
 
+        if (correctAnswer == 1)
+        {
+            StartCoroutine(blink.Blink(blink.answer1, blink.icon1, blink.text1));
+        }
+        if (correctAnswer == 2)
+        {
+            StartCoroutine(blink.Blink(blink.answer2, blink.icon2, blink.text2));
+        }
+        if (correctAnswer == 3)
+        {
+            StartCoroutine(blink.Blink(blink.answer3, blink.icon3, blink.text3));
+        }
+        if (correctAnswer == 4)
+        {
+            StartCoroutine(blink.Blink(blink.answer4, blink.icon4, blink.text4));
+        }
+
         correctAnswers.Remove(correctAnswer);
 
-        LoadNextQuestion();
+        Invoke("LoadNextQuestion", 3);
     }
 
     void LoadNextQuestion()
     {
+        blink.pause = false;
+
         if (GameManager.Instance.maxPlayerCount == 2)
         {
             player1.GetComponent<PlayerController>().answerChosed = 0;
@@ -129,7 +151,8 @@ public class AnswerManager : MonoBehaviour
         }
 
         int questionPicked = Random.Range(0, questionManager.questions.Count);
-        questionManager.QuestionWrite(questionManager.questions[questionPicked]);
+        StartCoroutine(questionManager.QuestionWrite(questionManager.questions[questionPicked]));
+        UpdateCorrectAnswer();
         answerTextManager.AnswerWrite(answerTextManager.firstAnswer[questionPicked],
             answerTextManager.secondAnswer[questionPicked],
             answerTextManager.thirdAnswer[questionPicked],
