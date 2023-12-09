92f13360d25c6f9f86b9f02438157b7bd892fd9a
Viper <41963246+SkullViper7@users.noreply.github.com>
2023-12-09 17:15:45 +0100

    Update LeaderboardManager.cs

--- Assets/Scripts/Quiz/Managers/LeaderboardManager.cs
+++ Assets/Scripts/Quiz/Managers/LeaderboardManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class LeaderboardManager : MonoBehaviour
 {
@@ -16,8 +17,12 @@ public class LeaderboardManager : MonoBehaviour
     public void ShowScore()
     {
         player1Score.text = scoreManager.player1Score.ToString();
+        MainLeaderboardManager.Instance.UpdateScore("QuizPlayer1", scoreManager.player1Score);
         player2Score.text = scoreManager.player2Score.ToString();
+        MainLeaderboardManager.Instance.UpdateScore("QuizPlayer2", scoreManager.player2Score);
         player3Score.text = scoreManager.player3Score.ToString();
+        MainLeaderboardManager.Instance.UpdateScore("QuizPlayer3", scoreManager.player3Score);
         player4Score.text = scoreManager.player4Score.ToString();
+        MainLeaderboardManager.Instance.UpdateScore("QuizPlayer4", scoreManager.player4Score);
     }
 }
